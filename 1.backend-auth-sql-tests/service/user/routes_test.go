e2ee6983adf0f49b12cf5ed2933cdf5c91cb7617
paruntapa <geniustechmax@gmail.com>
2025-12-16 23:37:03 +0530

    wrote three tests

--- 1.backend-auth-sql-tests/service/user/routes_test.go
+++ 1.backend-auth-sql-tests/service/user/routes_test.go
@@ -1,7 +1,26 @@
 package user
 
-import "testing"
+import (
+	"backend-apis/types"
+	"testing"
+)
+
+type mockUserStore struct{}
 
 func TestUserServiceHandler(t *testing.T) {
+	userStore := &mockUserStore{}
+	handler := NewHandler(userStore)
 
+}
+
+func (m *mockUserStore) GetUserByEmail(email string) (*types.User, error) {
+	return nil, nil
+}
+
+func (m *mockUserStore) GetUserByID(id int) (*types.User, error) {
+	return nil, nil
+}
+
+func (m *mockUserStore) CreateUser(types.User) error {
+	return nil
 }
